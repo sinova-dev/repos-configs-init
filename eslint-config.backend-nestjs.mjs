6a94f59305d530b4892f4718eafa02a3d49bce0d
George Rybko <hrybko.sinova@gmail.com>
2026-02-11 23:50:41 +0100

    adjust be config

--- eslint-config.backend-nestjs.mjs
+++ eslint-config.backend-nestjs.mjs
@@ -19,6 +19,17 @@ export function createBackendConfig(configDir) {
     eslintNestJs.configs.flatRecommended,
 
     ...createCoreRulesConfig(configDir),
+
+    {
+      files: ['**/*.ts'],
+      rules: {
+        // Crashes when factory functions are used with rest parameters. Can be turned on when the package fixes the issue.
+        '@darraghor/nestjs-typed/provided-injected-should-match-factory-parameters': 'off',
+        // Throws errors when using parameter property shorthands in constructor, which is very common in NestJS.
+        'erasable-syntax-only/parameter-properties': 'off',
+      },
+    },
+
     ...createCoreFinalConfig({ extraIgnores: backendIgnores }),
   );
 }
